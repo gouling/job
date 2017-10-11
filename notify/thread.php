bc64909d5015a9da0d0294479d050fb4e9e4b88b
gouling <gl.gouling@qq.com>
2017-10-11 14:53:42 +0800

    notify

--- notify/thread.php
+++ notify/thread.php
@@ -2,6 +2,8 @@
     /**
      * php 5.3.29 +
      * phar.readonly = Off
+     * {"action":"request","data":{"id":"gouling","tel":["17612800917","13458585242"]},"retry":"0"}
+     * {"action":"system","data":"stop/recovery"}
      */
     require 'notify.phar';
 
