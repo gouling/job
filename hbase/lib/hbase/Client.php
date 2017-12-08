198e23cd44f885a05b4f3caa50360c3c1e22f87d
gouling <gl.gouling@qq.com>
2017-12-09 00:30:57 +0800

    this is develop version

--- hbase/lib/hbase/Client.php
+++ hbase/lib/hbase/Client.php
@@ -18,49 +18,118 @@
             $this->__transport->open();
         }
 
-        public function delRow($table, $key) {
-            $this->__client->deleteAllRow($table, $key, []);
+        public function search($table, $startRow, $stopRow, $column = [], $nbRows = 10) {
+            $id = $this->__client->scannerOpenWithStop($table, $startRow, $stopRow, $column, []);
+            return $this->__client->scannerGet($id);
         }
 
-        public function addRow($data) {
-            foreach ($data['data'] as $column => $value) {
-                $this->__client->mutateRow($data['table'], $data['key'], [
-                    new Mutation([
-                        'column' => $column,
-                        'value' => $value
-                    ])
-                ], []);
+        /**
+         * 编辑行记录
+         * @param $table
+         * @param $key
+         * @param $data
+         */
+        public function setRow($table, $key, $data) {
+            $this->addRow($table, $key, $data);
+        }
+
+        /**
+         * 编辑行记录
+         * @param $data
+         */
+        public function setRows($data) {
+            foreach ($data as $v) {
+                $this->addRow($v['table'], $v['key'], $v['data']);
             }
         }
 
+        /**
+         * 添加行记录
+         * @param $table
+         * @param $key
+         * @param $data
+         */
+        public function addRow($table, $key, $data) {
+            $mutations = array();
+
+            foreach ($data as $column => $value) {
+                $mutations[] = new Mutation([
+                    'column' => $column,
+                    'value' => $value
+                ]);
+            }
+
+            $this->__client->mutateRow($table, $key, $mutations, []);
+        }
+
+        /**
+         * 添加行记录
+         * @param $data
+         */
         public function addRows($data) {
             foreach ($data as $v) {
-                $this->addRow($v);
+                $this->addRow($v['table'], $v['key'], $v['data']);
             }
         }
 
-        public function getRows($table) {
+        /**
+         * 查询行记录
+         * @param $table
+         * @param $key
+         * @return array
+         */
+        public function getRow($table, $key) {
             $list = [];
-            $k = 1;
-            while ($v = $this->__client->getRow($table, $k++, [])) {
+
+            if ($v = $this->__client->getRow($table, $key, [])) {
                 $v = array_pop($v);
 
                 foreach ($v->columns as $column => $value) {
-                    $list[$v->row][$column] = $value->value;
+                    $list[$column] = $value->value;
                 }
             }
 
             return $list;
         }
 
-        public function getTableNames() {
+        /**
+         * 查询行记录
+         * @param $table
+         * @return array
+         */
+        public function getRows($table) {
+            $list = [];
+            $k = 1;
+            while ($v = $this->getRow($table, $k, [])) {
+                $list[$k++] = $v;
+            }
+
+            return $list;
+        }
+
+        /**
+         * 删除行记录
+         * @param $table
+         * @param $key
+         */
+        public function delRow($table, $key) {
+            $this->__client->deleteAllRow($table, $key, []);
+        }
+
+        /**
+         * 查询表信息
+         * @param null $name
+         * @return array|mixed
+         */
+        public function getTableNames($name = null) {
             $desc = [];
             $tables = $this->__client->getTableNames();
+
             foreach ($tables as $table) {
                 $desc[$table] = $this->__client->getColumnDescriptors($table);
             }
 
-            return $desc;
+            return !is_null($name) && isset($desc[$name]) ? $desc[$name] : $desc;
         }
 
         public function __destruct() {
