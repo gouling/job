e582ea77b9933328702cc5b9e181c131ef0ac23f
gouling <gl.gouling@qq.com>
2018-02-14 00:04:31 +0800

    kafka

--- solr/index.php
+++ solr/index.php
@@ -2,7 +2,7 @@
     require('CZookeeper.php');
     require('CSolr.php');
 
-    $zookeeper = new CZookeeper('127.0.0.1:2181');
+    $zookeeper = new CZookeeper('127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183');
     $zookeeper->create(array(
         '/solr' => 'solr tender database',
         '/solr/host' => 'http://127.0.0.1:8983/solr/',
