1807c5cd21ecfb3405e330c2274ef3f612b5c8e6
gouling <gl.gouling@qq.com>
2018-04-13 15:50:51 +0800

    kafka

--- kafka/CProducer.php
+++ kafka/CProducer.php
@@ -16,6 +16,7 @@
         }
         
         public function send($data, $partition = RD_KAFKA_PARTITION_UA) {
+            //检查当前服务是否可用，不可用时延时再检查，依然不可用时必定服务全挂掉。此检查在批量操作时有性能影响。
             if($this->isRunning($this->__data['kafka']) == false) {
                 sleep($this->__data['timeout']);
                 if($this->isRunning($this->__data['kafka']) == false) {
@@ -30,6 +31,7 @@
              * 消息内容
              */
             $this->__topic->produce($partition, 0, $data);
+            //等待数据生产响应，此检查在批量操作时有性能影响。正常情况下无需等待可以提高效率
             while ($this->__kafka->getOutQLen() > 0) {
                 $this->__kafka->poll(1);
             }
