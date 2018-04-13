1e4b981236729a572436a4050374e734025afcf1
gouling <gl.gouling@qq.com>
2018-04-13 15:55:45 +0800

    kafka

--- kafka/index.php
+++ kafka/index.php
@@ -6,9 +6,9 @@
         
         '/application/kafka' => 'kafka服务，消费超时，延时微秒',
         '/application/kafka/host' => '192.168.253.170:9092',
-        '/application/kafka/log' => LOG_DEBUG,
-        '/application/kafka/timeout' => 1000,
-        '/application/kafka/delay' => 100000,
+        '/application/kafka/log' => LOG_DEBUG,  //接收数据时KAFKA日志级别
+        '/application/kafka/timeout' => 1000,   //接收数据超时（无数据，以微秒为单位）
+        '/application/kafka/delay' => 100000,   //收到数据处理后延时，防止CPU过高（以微秒为单位）
         
         '/application/kafka/tender' => '债权匹配，自动承接，用户提现，内部提现',
         '/application/kafka/tender/0'=>0,
