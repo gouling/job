06406d9167a784d3166511bc5c1df12fbc73dbb9
gouling <gl.gouling@qq.com>
2018-04-16 10:06:53 +0800

    kafka

--- kafka/consumer.php
+++ kafka/consumer.php
@@ -8,10 +8,12 @@
         }
     }
     
-    $kafka = new CConsumer(array(
+    $tender = array(
         'kafka' => '192.168.253.170:9092',
         'topic' => 'tender',
         'zk' => '192.168.253.170:2181',
         'root' => '/application/kafka',
-    ));
+    );
+    
+    $kafka = new CConsumer($tender);
     $kafka->accept(array(new Demo(), 'set'));
