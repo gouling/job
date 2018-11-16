67d4bda69f0b83dcee0f45195da6c3f424191e10
苟林 <gouling@goulindeMBP.lan>
2018-11-16 21:49:54 +0800

    mydreamplus

--- chat/index.php
+++ chat/index.php
@@ -2,32 +2,24 @@
     include_once __DIR__ . '/vendor/autoload.php';
     
     $chat = new \Chat\CChat();
-    $token = 'gt3TQjP_fCwsHMpCpUAabjD1Vu-GjOzgCPr1Col9TTyJX3PqLrdbjBRsfgjEUeIXYtCRgGT7G-aDAxCC05EN6emp6qpjvVeaqrrANFXMHPVSg340XDCYwwYzcwFqD2F04uxurye0tDgLLzd4mqE9z3GKVlU6AaHZaU1u8Yk80J1xewVYbFJVlLTkXH9LjwMwPeen-0KcxGW804F1dCcRmQ';
+    $token = file_get_contents('token.txt');
+    
     $department = $chat->getDepartmentList([
         1 => [
             'access_token' => $token,
         ]
     ]);
-
-    $department_user = $chat->getUserListByDepartmentId([
-        15 => [
-            'access_token' => $token,
-            'department_id' => 15,
-        ],
-        16 => [
-            'access_token' => $token,
-            'department_id' => 16,
-        ],
-        17 => [
-            'access_token' => $token,
-            'department_id' => 17,
-        ],
-        18 => [
-            'access_token' => $token,
-            'department_id' => 18,
-        ],
-    ]);
     
+    $get_user_req = [];
+    foreach ($department[1] as $k=>$v) {
+        $get_user_req[$v['id']] = [
+            'access_token' => $token,
+            'department_id' => $v['id'],
+        ];
+    }
+    
+    $department_user = $chat->getUserListByDepartmentId($get_user_req);
+
     $user = $chat->getUserInfoById([
         [
             'access_token' => $token,
@@ -38,6 +30,3 @@
             'user_id' => 'liyi',
         ],
     ]);
-    
-    print_r($department_user);
-    print_r($user);
