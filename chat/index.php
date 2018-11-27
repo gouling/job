c13e46bc2b12a6daf65ac6fe0e760a66d12fd63b
gouling <gl.gouling@qq.com>
2018-11-27 16:27:50 +0800

    chat

--- chat/index.php
+++ chat/index.php
@@ -1,15 +1,15 @@
 <?php
     include_once __DIR__ . '/vendor/autoload.php';
-    
+
     $chat = new \Chat\CChat();
-    $token = file_get_contents('token.txt');
-    
+    $token = 'adqzJI1gGDqDv-9QZY77nFezpJhcq1NwLsU6M5_iQ7Yts_gSoBWypS5wUQAjPeAYwnPl9hX1j-9ZPHTEEDDiHuKJ5_B7TnHTI-UdZOzInabtQAWMeHRcyzZgaF6SKYw51JES3pUZOIpRZpaJgpDJr1LmaQ2x3HaBz8ozZOCSOFD88cSfbiymXyHxvjgWORxMFO6jV7DpA2QY6RJKxZN54A';
+
     $department = $chat->getDepartmentList([
         1 => [
             'access_token' => $token,
         ]
     ]);
-    
+
     $get_user_req = [];
     foreach ($department[1] as $k=>$v) {
         $get_user_req[$v['id']] = [
@@ -23,10 +23,10 @@
     $user = $chat->getUserInfoById([
         [
             'access_token' => $token,
-            'user_id' => 'wuzhengyu',
+            'user_id' => 'liurongceshi',
         ],
         [
             'access_token' => $token,
-            'user_id' => 'liyi',
+            'user_id' => 'zhangqin',
         ],
     ]);
