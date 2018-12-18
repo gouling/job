1dec826bbaf14dddd3d222ebeaef08a31aa437f9
gouling <gl.gouling@qq.com>
2018-12-18 13:30:34 +0800

    chat

--- chat/index.php
+++ chat/index.php
@@ -1,32 +1,13 @@
 <?php
-    include_once __DIR__ . '/vendor/autoload.php';
-
-    $chat = new \Chat\CChat();
-    $token = 'adqzJI1gGDqDv-9QZY77nFezpJhcq1NwLsU6M5_iQ7Yts_gSoBWypS5wUQAjPeAYwnPl9hX1j-9ZPHTEEDDiHuKJ5_B7TnHTI-UdZOzInabtQAWMeHRcyzZgaF6SKYw51JES3pUZOIpRZpaJgpDJr1LmaQ2x3HaBz8ozZOCSOFD88cSfbiymXyHxvjgWORxMFO6jV7DpA2QY6RJKxZN54A';
-
-    $department = $chat->getDepartmentList([
-        1 => [
-            'access_token' => $token,
-        ]
-    ]);
-
-    $get_user_req = [];
-    foreach ($department[1] as $k=>$v) {
-        $get_user_req[$v['id']] = [
-            'access_token' => $token,
-            'department_id' => $v['id'],
-        ];
-    }
+    require('Chat.php');
+    require('Sync.php');
     
-    $department_user = $chat->getUserListByDepartmentId($get_user_req);
-
-    $user = $chat->getUserInfoById([
-        [
-            'access_token' => $token,
-            'user_id' => 'liurongceshi',
-        ],
-        [
-            'access_token' => $token,
-            'user_id' => 'zhangqin',
-        ],
-    ]);
+    $opts = [
+        'suite_id' => 'wxb388a3469a14426b',
+        'corp_id' => 'wwfd9b94c8c22a0bdf',
+        'token' => 'ni6bBmtw5Nwnil_Pw9IJejPNZRbZ9bJYovkJWcnnMwTqVOBmzWGvQY5Si9c9cIJ8HzHRnjf_m7PwHtXFogZeAr0K1zlCkucCiqgl9e5qXkCx1CQDdjb3JPfVDdfbfQVMp-hZ13dbNb5gOkfg8xp4hTbV4U0vmSaO6WOg29Zerk4_ez3EeUsT7vrt_YayYWvT00dTgazNuiYW_AFD0EsnLg',
+    ];
+    $sync = new Sync($opts);
+    print_r($sync->auth());
+    
+    
