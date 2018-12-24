17f7bf656e16c70eacd1945d16336ff32b493a64
gouling <gl.gouling@qq.com>
2018-12-24 17:06:30 +0800

    chat

--- chat/synchronizes/Sync.php
+++ chat/synchronizes/Sync.php
@@ -9,12 +9,16 @@
         public function cancel() {
             $trans = [
                 "DELETE FROM chat_ent WHERE id='{$this->opts['id']}'",
+                
                 "DELETE FROM level_user USING level_user,chat_level WHERE chat_level.ent_id='{$this->opts['id']}' AND level_user.level_id=chat_level.level_id",
                 "DELETE FROM level_user USING level_user,chat_user WHERE chat_user.ent_id='{$this->opts['id']}' AND level_user.user_id=chat_user.user_id",
+                
                 "DELETE FROM level USING level,chat_level WHERE chat_level.ent_id='{$this->opts['id']}' AND level.id=chat_level.level_id",
                 "DELETE FROM chat_level WHERE ent_id='{$this->opts['id']}'",
+                
                 "DELETE FROM user USING user,chat_user WHERE chat_user.ent_id='{$this->opts['id']}' AND user.id=chat_user.user_id",
                 "DELETE FROM chat_user WHERE chat_user.ent_id='{$this->opts['id']}'",
+                
                 "ALTER TABLE level AUTO_INCREMENT=1",
                 "ALTER TABLE user AUTO_INCREMENT=1",
             ];
